050a62f4fa457eaff020c57c427f43ca2099224a
Aslam p a <93756533+LW1117@users.noreply.github.com>
2023-06-11 14:44:34 +0530

    Update forms.py
    removed unnecessary lines

--- base/forms.py
+++ base/forms.py
@@ -1,6 +1,1 @@
-from django import forms
-from . models import Location
-class LocationForm(forms.ModelForm):
-    class Meta:
-        model = Location
-        fields = ['name', 'latitude', 'longitude']
+
